0f8ed103ddc79fc3b82960948ac521a0c9309939
home <noiseyou99.cs00g@g2.nctu.edu.tw>
2018-07-14 16:01:54 +0800

    First time upload.

---
+++ LineBotSDK_Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LineBotSDK;
+
+namespace LineBotSDK_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "";
+            string userid = "";
+            string image_url = "https://i0.wp.com/blog.patw.me/wp-content/uploads/2017/05/ZcNMMLg.png?fit=800%2C416&ssl=1";
+
+            LineBotSDK.Utility.MessageUtility.PushTextMessage(token, userid, "Hi");
+
+            LineBotSDK.Utility.MessageUtility.PushImageMessage(token, userid, image_url, image_url);
+        }
+    }
+}
