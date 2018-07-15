821390daf14371e30c5b1a46454027574924c19f
home <noiseyou99.cs00g@g2.nctu.edu.tw>
2018-07-15 11:03:16 +0800

    Add the nuget to include packages. Add the PushVideoMessage and PushStickerMessage.

--- LineBotSDK_Example/Program.cs
+++ LineBotSDK_Example/Program.cs
@@ -11,13 +11,23 @@ namespace LineBotSDK_Example
     {
         static void Main(string[] args)
         {
-            string token = "";
-            string userid = "";
+            string token = ""; // please input the line bot token
+            string userid = "";// please input the user id
             string image_url = "https://i0.wp.com/blog.patw.me/wp-content/uploads/2017/05/ZcNMMLg.png?fit=800%2C416&ssl=1";
+            string mp4_url = "https://www.legacyvet.com/sites/default/files/videos/Video%20%281%29.mp4";
 
+            // Send the text message to user
             MessageUtility.PushTextMessage(token, userid, "Hi");
 
+            // Send the image message to user
             MessageUtility.PushImageMessage(token, userid, image_url, image_url);
+
+            // Send the video message to user
+            MessageUtility.PushVideoMessage(token, userid, mp4_url, image_url);
+
+            // Send the sticker message to user
+            MessageUtility.PushStickerMessage(token, userid, "1", "1");
+            
         }
     }
 }
